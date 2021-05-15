92cc521f2931df5c63c2976377071f9fb92383cb
EremitaNativeSound <36335229+Protodiv@users.noreply.github.com>
2021-05-15 22:09:50 +0300

    22

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
  if('serviceWorker' in navigator){
-    navigator.serviceWorker.register("js/sw.js")
+    navigator.serviceWorker.register("js/pwabuilder-sw.js")
         .then(() => console.log("Зарегистрировали"))
         .catch(() => console.log("ошибка"))
 }
+
